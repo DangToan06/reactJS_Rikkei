feb3de60bd6dba30f0edb5348976c54c71b35de8
dangtoan <dangtoan.2k6@gmail.com>
2025-08-25 08:20:13 +0700

    ss15

--- PTIT_CNTT1_IT104_Session15/src/components/ManagerStudent/Pagination.tsx
+++ PTIT_CNTT1_IT104_Session15/src/components/ManagerStudent/Pagination.tsx
@@ -1,7 +1,37 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import React, { Component } from "react";
+import "./Pagination.css";
 
-export default class Pagination extends Component {
+type StateType = {
+  currentPage: number;
+  pageSize: number;
+};
+
+export default class Pagination extends Component<object, StateType> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      pageSize: 5,
+    };
+  }
   render() {
-    return <div>Pagination</div>;
+    return (
+      <div className="container">
+        <div className="btn-pre">
+          <ChevronLeft />
+        </div>
+        <div className="container-pagination">
+          <div className="num-page current">1</div>
+          <div className="num-page">2</div>
+          <div className="num-page">3</div>
+          <div className="num-page">4</div>
+          <div className="num-page">5</div>
+        </div>
+        <div className="btn-next">
+          <ChevronRight />
+        </div>
+      </div>
+    );
   }
 }
